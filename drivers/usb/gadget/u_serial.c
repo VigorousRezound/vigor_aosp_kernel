eb8921a729fec450bf262572e6c869ce431b6b11
mrg666 <drgungor@hotmail.com>
2013-05-13 10:33:32 -0400

    disable CONFIG_DEBUG_FS and open the pandora's box While fixing the mess spilled out of the box, all code changes are my fault, don't blame anyone else :) To the devs who made this mess: A kernel option is an option, you must know that it can be disabled. If you need a kernel option, declare a dependence in one of the Kconfig files. Otherwise, put your "professional" coding together and handle the optional kernel features properly. DEBUG_FS is not your scratch pad. Sheesh! -mrg666
    Change-Id: I1f3aabd0f50f7f3287209eec1c4af04bfd3174aa

--- drivers/usb/gadget/u_serial.c
+++ drivers/usb/gadget/u_serial.c
@@ -1314,8 +1314,8 @@ static void usb_debugfs_init(struct gs_port *ui_dev, int port_num)
 	debugfs_create_file("readstatus", 0444, dent, ui_dev, &debug_adb_ops);
 	debugfs_create_file("reset", 0222, dent, ui_dev, &debug_rst_ops);
 }
-
-static void usb_debugfs_init(struct gs_port *ui_dev) {}
+#else
+static void usb_debugfs_init(struct gs_port *ui_dev, int port_num) {}
 #endif
 
 /**
